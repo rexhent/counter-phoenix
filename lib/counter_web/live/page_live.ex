eb02cac92aebaed06ea85ccef517d61531d904c6
Dominic Hannah <dominic.hannah@education.nsw.gov.au>
2024-05-23 22:35:19 +1000

    first

---
+++ lib/counter_web/live/page_live.ex
@@ -0,0 +1,25 @@
+defmodule CounterWeb.PageLive do
+  use CounterWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(number: 0)}
+  end
+
+  def handle_event("increment", _params, socket) do
+    {:noreply,
+     socket
+     |> update(:number, &(&1 + 1))}
+  end
+  def handle_event("decrement", _params, socket) do
+    {:noreply,
+     socket
+     |> update(:number, &max(0, &1 - 1))}
+  end
+  def handle_event("reset", _params, socket) do
+    {:noreply,
+     socket
+     |> assign(number: 0)}
+  end
+end
